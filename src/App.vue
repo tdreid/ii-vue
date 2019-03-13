e5d69200ad2e2e7dd7bf30887ff3412f00a15717
tdreid <trevor.d.reid@gmail.com>
2019-03-13 02:52:26 +0000

    Remove HelloWorld instance from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
+    <script src=".background/background.js"></script>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
